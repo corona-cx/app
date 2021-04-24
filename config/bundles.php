422aae570314843bda1818606b7653f471f679d2
Malte Hübner <maltehuebner@gmx.org>
2021-04-24 13:17:26 +0200

    Setup twig and first controller

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
-<?php declare(strict_types=1);
+<?php
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
