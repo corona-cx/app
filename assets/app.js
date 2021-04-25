1558a7e58a4c4aa93d0f28725b8f30f52076c0bb
Malte Hübner <maltehuebner@gmx.org>
2021-04-25 19:50:48 +0200

    Add font awesome

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,15 @@
 /*
  * Welcome to your app's main JavaScript file!
- *
+ *npm config set "@fortawesome:registry" https://npm.fontawesome.com/
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+import '@fortawesome/fontawesome-pro/js/all';
+import '@fortawesome/fontawesome-pro/js/fontawesome.min';
 
 // start the Stimulus application
 import './bootstrap';
