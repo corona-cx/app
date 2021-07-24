61cca29d85495ef25a0a14a33e452b592ee97059
Malte Hübner <maltehuebner@gmx.org>
2021-07-25 01:02:54 +0200

    WIP for coords.

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -3,15 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisplayController extends AbstractController
 {
     #[Route('/coords', name: 'display_coords', options: ['expose' => true])]
-    public function displayCoords(): Response
+    public function displayCoords(Request $request): Response
     {
-        return $this->render('frontpage/index.html.twig');
+        $latitude = (float) $request->query->get('latitude');
+        $longitude = (float) $request->query->get('longitude');
+
+        $apiUrl = sprintf('https://localhost:8000/?latitude=%f&longitude=%f', $latitude, $longitude);
+
+        $content = file_get_contents($apiUrl);
+
+        $jsonContent = json_decode($content);
+
+        $areaName = $jsonContent->area->name;
+        $dateTime = new \DateTime($jsonContent->date_time);
+        $incidence = $jsonContent->cases7_per100k;
+
+        return $this->render('display/coord.html.twig', [
+            'area_name' => $areaName,
+            'date_time' => $dateTime,
+            'incidence' => $incidence,
+        ]);
     }
 
     #[Route('/{slug}', name: 'display_city', options: ['expose' => true])]
